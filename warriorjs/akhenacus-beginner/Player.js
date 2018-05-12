97fe9c2c6538c7dc72b17abcbb2acdfe02a53db1
Unknown <andre.leruitte@gmail.com>
2018-05-13 01:21:35 +0200

    level 3 finished

--- warriorjs/akhenacus-beginner/Player.js
+++ warriorjs/akhenacus-beginner/Player.js
@@ -1,9 +1,24 @@
 class Player {
+  constructor() {
+    this.MAX_HEALTH = 20;    
+  }
+
   playTurn(warrior) {
-    if (warrior.feel().isEmpty()) {
-    warrior.walk();
-    } else {
+    if (this.isHostileAhead(warrior)) {
       warrior.attack();
+    } else if (this.needsRest(warrior)) {
+      warrior.rest();
+    } else warrior.walk();    
+  }
+
+  needsRest(warrior) {
+    if (warrior.health() < (this.MAX_HEALTH)) {
+      return true;
     }
+    return false;
+  }
+
+  isHostileAhead(warrior) {
+    return !warrior.feel().isEmpty();
   }
 }
