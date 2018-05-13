0ae774c5250205fea4c85574d0eb7e4f0676617f
Unknown <andre.leruitte@gmail.com>
2018-05-13 02:31:21 +0200

    level 6 finished

--- warriorjs/akhenacus-beginner/Player.js
+++ warriorjs/akhenacus-beginner/Player.js
@@ -1,37 +1,83 @@
 class Player {
   constructor() {
     this.MAX_HEALTH = 20;
-    this.lastTurnHealth = 20;  
+    this.HEALING_THREASHOLD = 0.5;  //warrior needs to rest when HP belown 80%
+
+    this.lastTurnHealth = 20;
+    this.allDirections = ["forward", "backward", "left", "right"];
+    this.turnNumber = 0;
   }
 
   playTurn(warrior) {
-    if (this.isHostileAhead(warrior)) {
-      warrior.attack();
+    if (this.turnNumber == 0) {
+      warrior.walk("backward");
+    } else if (this.isTakingDamage && this.isLiveCritical(warrior)) {
+      this.retreat(warrior);
+    } else if (this.isHealing(warrior) && warrior.health() < this.MAX_HEALTH) {
+      /* once warrior starts healing, he will heal until full health */
+      warrior.rest();
+    } else if (this.getHostileAround(warrior) != undefined) {
+      warrior.attack(this.getHostileAround(warrior));
     } else if (this.needsRest(warrior) && !this.isTakingDamage(warrior)) {
       warrior.rest();
-    } else if (this.isCaptiveAhead(warrior)) {
-      warrior.rescue();
-    } else warrior.walk();
+    } else if (this.getCaptiveAround(warrior) != undefined) {
+      warrior.rescue(this.getCaptiveAround(warrior));
+    } else this.moveInBestDirection(warrior);
 
     this.lastTurnHealth = warrior.health();
+    this.turnNumber++;
+  }
+
+  moveInBestDirection(warrior) {
+    /*if (this.isTakingDamage(warrior)) warrior.walk("backward");
+    else */warrior.walk();
   }
 
   needsRest(warrior) {
-    if (warrior.health() < (this.MAX_HEALTH)) {
+    if (warrior.health() < (this.MAX_HEALTH * this.HEALING_THREASHOLD)) {
       return true;
     }
     return false;
   }
 
-  isHostileAhead(warrior) {
-    return !warrior.feel().isEmpty() && !warrior.feel().isCaptive();
+  getHostileAround(warrior) {
+    var foundDirection = this.allDirections.find(
+      direction => {
+        return (!warrior.feel(direction).isEmpty() 
+        && !warrior.feel(direction).isWall()
+        && !warrior.feel(direction).isCaptive())
+      });
+    return foundDirection;
   }
 
   isTakingDamage(warrior) {
     return warrior.health() < this.lastTurnHealth;
   }
 
-  isCaptiveAhead(warrior) {
-    return warrior.feel().isCaptive();
+  isHealing(warrior) {
+    return warrior.health() > this.lastTurnHealth;
   }
+
+  getCaptiveAround(warrior) {
+    var foundDirection = this.allDirections.find(
+      direction => {
+        return (!warrior.feel(direction).isEmpty()
+        && warrior.feel(direction).isCaptive());
+      });
+    return foundDirection;
+  }
+
+  isLiveCritical(warrior) {
+    return warrior.health() < (this.MAX_HEALTH / 4);
+  }
+
+  retreat(warrior) {
+    if (warrior.feel("backward").isEmpty()) {
+      warrior.walk("backward");
+    } else {
+      warrior.rest();
+    }
+  }
+
+
 }
--- warriorjs/akhenacus-beginner/README.md
+++ warriorjs/akhenacus-beginner/README.md
@@ -1,31 +1,32 @@
-## Akhenacus - beginner - level 5
+## Akhenacus - beginner - level 6
 
-### _You hear cries for help. Captives must need rescuing._
+### _The wall behind you feels a bit further away in this room. And you hear more cries for help._
 
-> **TIP:** Use `warrior.feel().isCaptive()` to see if there's a captive and `warrior.rescue()` to rescue him. Don't attack captives.
+> **TIP:** You can walk backward by passing `'backward'` as an argument to `walk()`. Same goes for `feel()`, `rescue()` and `attack()`. Archers have a limited attack distance.
 
+> **CLUE:** Walk backward if you're taking damage from afar and don't have enough health to attack. You may also want to consider walking backward until you hit a wall. Use `warrior.feel().isWall()` to see if there's a wall.
 
 ### Floor Map
 
 ```
-╔═══════╗
-║@ CaaSC║
-╚═══════╝
+╔════════╗
+║C @ S aa║
+╚════════╝
 
-@ = Akhenacus (20 HP)
 C = Captive (1 HP)
-a = Archer (7 HP)
+@ = Akhenacus (20 HP)
 S = Thick Sludge (24 HP)
+a = Archer (7 HP)
 ```
 
 ### Abilities
 
 #### Actions (only one per turn)
 
-* `warrior.rescue()`: Rescue a captive from his chains (earning 20 points) in the given direction (forward by default).
 * `warrior.walk()`: Move one space in the given direction (forward by default).
 * `warrior.attack()`: Attack a unit in the given direction (forward by default) dealing 5 HP of damage.
 * `warrior.rest()`: Gain 10% of max health back, but do nothing more.
+* `warrior.rescue()`: Rescue a captive from his chains (earning 20 points) in the given direction (forward by default).
 
 #### Senses
 
